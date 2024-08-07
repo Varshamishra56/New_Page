0410ded557ded5c258eaf277bc64d8b042a95303
Varshamishra56 <vm0222912@gmail.com>
2024-08-07 23:39:55 +0530

    page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'primary':"#ee1081",
+        'Varsha':"#eee000"
+      }
+    },
+  },
+  plugins: [],
+}
+
